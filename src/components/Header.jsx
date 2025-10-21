1ac974d6bbb789ec672e8b52e742dd344e036c16
theumarov62 <maxbubaxonxoliqova62@gmail.com>
2025-10-21 22:58:42 +0500

    add ReactProject 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="Container HeaderContainer">
+          <a className="HeaderTitleContainer" href="../../index.html">
+            <img
+              src="./logo-icon.svg"
+              alt="Logo Web Site"
+              className="HeaderTitleLogo"
+            />
+            <span className="HeaderTitleText">AnsorMed</span>
+          </a>
+
+          {/* Header Nav */}
+          <nav className="Nav">
+            <li className="links">
+              <a className="link" href="#">
+                Xizmatlar
+              </a>
+            </li>
+            <li className="links">
+              <a className="link" href="#">
+                Dorilar
+              </a>
+            </li>
+            <li className="links">
+              <a className="link" href="#">
+                Asal
+              </a>
+            </li>
+            <li className="links">
+              <a className="link" href="#">
+                Kontaktlar
+              </a>
+            </li>
+            <li className="links">
+              <a className="link" href="#">
+                Blog
+              </a>
+            </li>
+          </nav>
+
+        
+
+          {/* Header Register */}
+          <button className="HeaderButtonRegister">Qabulga yozilish</button>
+        </div>
+      </header>
+    </>
+  );
+}
